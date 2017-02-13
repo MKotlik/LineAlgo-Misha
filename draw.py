63470725b2c4979af6b3bc396152ea862b8ee9e2
Mikhail Kotlik <mkotlik@stuy.edu>
2017-02-13 11:46:03 -0500

    finished o2, working on o8

--- draw.py
+++ draw.py
@@ -15,3 +15,35 @@ def draw_line(x0, y0, x1, y1, screen, color):
             d += 2*B
         x += 1
         d += 2*A
+
+
+def draw_line_o2(x0, y0, x1, y1, screen, color):
+    # Set initial coords & calculate line constants
+    x = x0
+    y = y0
+    A = y1 - y0
+    B = x0 - x1  # -(x1 - x0)
+    d = A + 2*B
+    while (y <= y1):
+        plot(screen, color, x, y)
+        if d < 0:
+            x += 1
+            d += 2*A
+        y += 1
+        d += 2*B
+
+
+def draw_line_o8(x0, y0, x1, y1, screen, color):
+    # Set initial coords & calculate line constants
+    x = x0
+    y = y0
+    A = y1 - y0
+    B = x0 - x1  # -(x1 - x0)
+    d = 2*A - B
+    while (x <= x1):
+        plot(screen, color, x, y)
+        if d > 0:
+            y -= 1
+            d -= 2*B
+        x += 1
+        d += 2*A
--- main.py
+++ main.py
@@ -11,5 +11,13 @@ draw_line(1, 1, 500, 1, screen, color)
 draw_line(50, 50, 200, 150, screen, color)
 draw_line(100, 60, 400, 200, screen, color)
 
+
+# Octant2 Test #
+color[RED] = 255
+color[GREEN] = 0
+draw_line_o2(0, 0, 0, 499, screen, color)
+draw_line_o2(0, 0, 10, 250, screen, color)
+draw_line_o2(0, 0, 50, 300, screen, color)
+
 display(screen)
 save_extension(screen, 'img.png')
