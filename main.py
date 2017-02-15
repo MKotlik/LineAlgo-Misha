46c2a714d41ff00a646f506b1feb54f49b7f2ab1
MKotlik <mkotlik@stuy.edu>
2017-02-15 11:48:32 -0500

    Finished draw_line wrapper

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
+"""
 # Test Code #
 draw_line_octant1(0, 0, 499, 499, screen, color)
 draw_line_octant1(1, 1, 100, 10, screen, color)
@@ -18,6 +19,18 @@ color[GREEN] = 0
 draw_line_o2(0, 0, 0, 499, screen, color)
 draw_line_o2(0, 0, 10, 250, screen, color)
 draw_line_o2(0, 0, 50, 300, screen, color)
+
+# Octant7 Test #
+color[GREEN] = 0
+color[BLUE] = 255
+draw_line_octant7(0,500,200,100, screen, color)
+
+# Octant8 Test #
+color[GREEN] = 255
+color[BLUE] = 0
+draw_line_octant8(0,500,400,400, screen, color)
+"""
+
 
 display(screen)
 save_extension(screen, 'img.png')
