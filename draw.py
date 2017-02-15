76fdd82d7b6d4797b047e2385c779688defa92de
MKotlik <mkotlik@stuy.edu>
2017-02-15 06:29:28 -0500

    Finished base octant algos

--- draw.py
+++ draw.py
@@ -2,6 +2,12 @@ from display import *
 
 
 def draw_line(x0, y0, x1, y1, screen, color):
+    # General draw_line wrapper
+    # Decides which octant_helper to call, modifies starting-ending points
+    pass
+
+
+def draw_line_octant1(x0, y0, x1, y1, screen, color):
     # Set initial coords & calculate line constants
     x = x0
     y = y0
@@ -17,7 +23,7 @@ def draw_line(x0, y0, x1, y1, screen, color):
         d += 2*A
 
 
-def draw_line_o2(x0, y0, x1, y1, screen, color):
+def draw_line_octant2(x0, y0, x1, y1, screen, color):
     # Set initial coords & calculate line constants
     x = x0
     y = y0
@@ -33,7 +39,23 @@ def draw_line_o2(x0, y0, x1, y1, screen, color):
         d += 2*B
 
 
-def draw_line_o8(x0, y0, x1, y1, screen, color):
+def draw_line_octant7(x0, y0, x1, y1, screen, color):
+    # Set initial coords & calculate line constants
+    x = x0
+    y = y0
+    A = y1 - y0
+    B = x0 - x1  # -(x1 - x0)
+    d = A - 2*B
+    while (y >= y1):
+        plot(screen, color, x, y)
+        if d > 0:
+            x += 1
+            d += 2*A
+        y -= 1
+        d -= 2*B
+
+
+def draw_line_octant8(x0, y0, x1, y1, screen, color):
     # Set initial coords & calculate line constants
     x = x0
     y = y0
@@ -42,7 +64,7 @@ def draw_line_o8(x0, y0, x1, y1, screen, color):
     d = 2*A - B
     while (x <= x1):
         plot(screen, color, x, y)
-        if d > 0:
+        if d < 0:
             y -= 1
             d -= 2*B
         x += 1
--- main.py
+++ main.py
@@ -5,11 +5,11 @@ screen = new_screen()
 color = [ 0, 255, 0 ]
 
 # Test Code #
-draw_line(0, 0, 499, 499, screen, color)
-draw_line(1, 1, 100, 10, screen, color)
-draw_line(1, 1, 500, 1, screen, color)
-draw_line(50, 50, 200, 150, screen, color)
-draw_line(100, 60, 400, 200, screen, color)
+draw_line_octant1(0, 0, 499, 499, screen, color)
+draw_line_octant1(1, 1, 100, 10, screen, color)
+draw_line_octant1(1, 1, 500, 1, screen, color)
+draw_line_octant1(50, 50, 200, 150, screen, color)
+draw_line_octant1(100, 60, 400, 200, screen, color)
 
 
 # Octant2 Test #
